8cb48979856fde35b37e1de9cf0b227c03744b2a
delapaska <37419828+delapaska@users.noreply.github.com>
2022-01-13 15:56:41 +0300

    Add files via upload

---
+++ sed is for losers.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+int main()
+{
+	std::string filename, s1, s2;
+	std::cout << "Enter the filename: ";
+	std::cin >> filename;
+	filename = filename + ".replace";
+	std::ofstream file;
+	file.open(filename);
+	s1 = "First string";
+	s2 = "Second string";
+	file << s1 << std::endl << s2;
+	file.close();
+}
+
